1222e2f3095f1b1b07240ea24d587919f9827b45
Víctor Tamayo <victortamayo509@gmail.com>
2022-07-12 15:37:52 -0500

    views

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -4,4 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello Wordl Premios Platzi")
+    return HttpResponse("Estas en la pagina principal de Premios Platzi")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta numero {question_id}")
