538e84edf05db1134450f9065dd694e0f424cccc
Víctor Tamayo <victortamayo509@gmail.com>
2022-07-12 14:21:48 -0500

    Choice model

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,5 +1,11 @@
+from secrets import choice
 from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateField("date published")
+
+class Choices(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
